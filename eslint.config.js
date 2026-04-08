0f09a249513644000dabd7ab89d6599f795a1b92
Yoann Ciabaud <yoann@linxo.com>
2026-04-08 10:52:54 +0200

    fix: update nodejs version and migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from "typescript-eslint";
+import eslint from "@eslint/js";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+    }
+  },
+  {
+    ignores: ["build/", "node_modules/", "dist/", "eslint.config.js"]
+  }
+);
